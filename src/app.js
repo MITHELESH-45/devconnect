83aba5485541892d59b85686ca2e21e735633aa0
Mithelesh-45 <mitheleshk3005@gmail.com>
2026-02-05 09:49:46 +0530

    created a express server

--- src/app.js
+++ src/app.js
@@ -5,7 +5,9 @@ const app=express();
 app.use("/hi",(req,res)=>{
     res.send('Hello World');
 });
-
+app.use("/bye",(req,res)=>{
+    res.send('Goodbye World');
+}
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
