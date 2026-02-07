91325b9b4bb1c9cdd8a3f8a7974bc348481fe164
Mithelesh-45 <mitheleshk3005@gmail.com>
2026-02-07 09:40:28 +0530

    created User Modal

--- src/app.js
+++ src/app.js
@@ -1,7 +1,59 @@
 const express=require('express');
 const connectDb=require('./config/database');
+const User=require('./models/user');
 const app=express();
+app.use(express.json());
 
+app.post("/signup",async (req,res)=>{
+    console.log(req.body);
+     try{
+    const user=new User(req.body);
+    await user.save()
+    console.log("User saved successfully");
+    res.send("user stored in db");
+
+    }catch(err){
+        console.error("Error saving user:", err);
+        res.status(500).send("Error saving user");
+     
+    }
+});
+
+app.get("/feed",async (req,res)=>{
+
+    const email=req.query.email;
+
+    try{
+        const user=await User.findOne({email:email});
+        res.send(user);
+    }catch(err){
+        console.log(err);
+        res.status(500).send("Error fetching user");
+    }
+});
+
+app.delete("/user",async(req,res)=>{
+    const id=req.body.id;
+    try{
+        await User.findByIdAndDelete(id);
+        res.send("User deleted successfully");
+    }catch(err){
+        console.log(err);
+        res.status(500).send("Error deleting user");
+    }
+});
+
+app.patch("/user",async(req,res)=>{
+    const id=req.body.id;
+    const data=req.body;
+    try{
+        const user=await User.findByIdAndUpdate(id,data);
+        res.send("User updated successfully");
+    }catch(err){
+        console.log(err);
+        res.status(500).send("Error updating user");
+    }
+});
 
 connectDb().then(()=>{
       console.log("Database connected successfully");
@@ -11,6 +63,8 @@ connectDb().then(()=>{
 }).catch((error)=>{
       console.error("Database connection failed:", error);
 });
+
+
 
 
 
