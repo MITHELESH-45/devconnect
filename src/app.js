f6ac6c795acc5be13947361dba1391eac3ea461d
Mithelesh-45 <mitheleshk3005@gmail.com>
2026-02-10 21:49:48 +0530

    created login api

--- src/app.js
+++ src/app.js
@@ -1,21 +1,38 @@
 const express=require('express');
 const connectDb=require('./config/database');
 const User=require('./models/user');
+const bcrypt=require('bcrypt');
+const {validateSignUpData,validateEmail}=require('./utils/validate');
+
+
 const app=express();
 app.use(express.json());
 
 app.post("/signup",async (req,res)=>{
-    console.log(req.body);
-    const data=req.body;
-     try{
+    try{
+    //validation of data
+      validateSignUpData(req);
+    
+    //encrption of password
+     const password=req.body.password;
+     const hashedPassword=await bcrypt.hash(password,10);
+     req.body.password=hashedPassword;
 
-      if(data.skills && data.skills.length>10){
-            throw new Error("Skills should be less than 10");
-        }
-    const user=new User(req.body);
-    await user.save()
-    console.log("User saved successfully");
-    res.send("user stored in db");
+        const{firstName,lastName,email}=req.body;
+        const user=new User({
+            firstName,
+            lastName,
+            email,
+            password:hashedPassword,
+            skills:req.body.skills || [],
+            age:req.body.age,
+            gender:req.body.gender,
+            about:req.body.about,
+            photoUrl:req.body.photoUrl
+        });
+        await user.save()
+        console.log("User saved successfully");
+        res.send("user stored in db");
 
     }catch(err){
         console.error("Error saving user:", err);
@@ -23,6 +40,28 @@ app.post("/signup",async (req,res)=>{
      
     }
 });
+
+app.get("/login",async (req,res)=>{
+
+    try{
+        const {email,password}=req.body;
+        validateEmail(email);
+
+        const user=await User.findOne({email:email});
+        if(!user){
+            return res.status(400).send("Invalid credentials");
+        }
+        const passwordMatch=await bcrypt.compare(password,user.password);
+        if(!passwordMatch){
+            return res.status(400).send("Invalid credentials");
+        }
+        res.send("Login successful");
+    }catch(err){
+        console.error("Error validating email:", err);
+        res.status(400).send("Invalid email address");
+    }
+});
+
 
 app.get("/feed",async (req,res)=>{
 
---
+++ src/utils/validate.js
@@ -0,0 +1,33 @@
+const validator=require("validator");
+
+const validateSignUpData=(req)=>{
+    const{firstName,lastName,email,password,skills}=req.body;
+
+    if(!firstName||!lastName){
+        throw new Error("First name and last name are required");
+    }
+    else if(!validator.isEmail(email)){
+        throw new Error("Invalid email address");
+    }
+    else if(!validator.isStrongPassword(password)){
+        throw new Error("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one symbol");
+    }
+    else if(skills && skills.length>10){
+        throw new Error("Skills should be less than 10");
+    }
+    return true;
+
+}
+
+const validateEmail=(email)=>{
+     
+    if(!validator.isEmail(email)){
+        throw new error("Invalid email address");
+    }
+    return true;
+}
+
+
+module.exports={
+    validateSignUpData,validateEmail
+};
