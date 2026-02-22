814313e101cd680a72b691972d741e541e954ae4
Mithelesh-45 <mitheleshk3005@gmail.com>
2026-02-22 16:27:46 +0530

    fixed cors issue

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -53,8 +53,12 @@ authRouter.post("/login",async (req,res)=>{
         }
         
         const token= user.toJWT();
-        res.cookie("token",token,{expires:new Date(Date.now()+7*24*60*60*1000)});
-        res.send("Login successful");
+        res.cookie("token",token,{
+            httpOnly: true,
+            secure: false,
+            sameSite: "none",
+            expires:new Date(Date.now()+7*24*60*60*1000)});
+        res.json({message:"Login successful",data:user});
 
     }catch(err){
         console.error("Error validating email:", err);
