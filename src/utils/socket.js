6c06d467a62030571f1d414be5a43720fefd324f
Mithelesh-45 <mitheleshk3005@gmail.com>
2026-07-08 18:19:10 +0530

    feat: add chat model to handle message structure and persistence

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -19,15 +19,44 @@ const intializeSocket = (server) => {
 
         socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
 
-            const roomId = getSecretRoomId(userId, targetUserId);
+            const roomId = getSecretRoomId({ userId, targetUserId });
 
             socket.join(roomId);
         });
 
-        socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+        socket.on("sendMessage", async ({ firstName, userId, targetUserId, text }) => {
 
-            const roomId = getSecretRoomId(userId, targetUserId);
-            io.to(roomId).emit("messageReceived", { firstName, text });
+
+            try {
+
+                const roomId = getSecretRoomId(userId, targetUserId);
+
+                let chat = await Chat.findOne({
+                    participants: {
+                        $all: [userId, targetUserId]
+                    }
+                });
+
+                if (!chat) {
+
+                    chat = new Chat({
+                        participants: [userId, targetUserId],
+                        messages: []
+                    })
+                }
+
+                chat.messages.push({
+                    senderId: userId,
+                    text,
+                });
+
+                await chat.save();
+
+                io.to(roomId).emit("messageReceived", { firstName, text });
+
+            } catch (err) {
+                console.log(err.message);
+            }
 
         });
 
